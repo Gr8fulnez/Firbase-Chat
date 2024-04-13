1ef830736e3ab24d3542682edd6a6532cdd43fda
Gr8fulnez <oluwolejohn72@gmail.com>
2024-04-12 22:19:31 -0700

    chat layout setup

---
+++ src/components/list/List.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./list.css";
+import Userinfo from "./Userinfo/Userinfo";
+import ChatList from "./chatList/ChatList";
+const List = () => {
+  return (
+    <div className="list">
+      <Userinfo />
+      <ChatList />
+    </div>
+  );
+};
+
+export default List;
