3a7e428083233a8526cb99dff8fd0ce41b1980a8
Gr8fulnez <oluwolejohn72@gmail.com>
2024-04-15 05:23:56 -0700

    setup zustand as statemanagement

--- src/components/list/Userinfo/Userinfo.jsx
+++ src/components/list/Userinfo/Userinfo.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import "./userInfo.css";
+import { useUserStore } from "../../../lib/userStore";
 
 const Userinfo = () => {
+
+  const { currentUser } = useUserStore();
+
+  console.log(currentUser)
   return (
     <div className="userInfo">
       <div className="user">
-        <img src="./avatar.png" alt="" />
-        <p>John Doe</p>
+        <img src={currentUser.avatar || "./avatar.png"} alt="" />
+        <p>{currentUser.username}</p>
       </div>
       <div className="icons">
         <img src="./more.png" alt="" />
